110d7155ce9f46f1e4cc4e1135c1660ff8817aa9
Deepanshu Jindal <deepu.jindal2002@gmail.com>
2021-05-19 11:12:24 +0530

    login added

---
+++ src/templates/forms/login.js
@@ -0,0 +1,139 @@
+import React,{Component} from 'react';
+import {connect} from 'react-redux';
+import {AiOutlineMail} from 'react-icons/ai';
+import {RiLockPasswordFill} from 'react-icons/ri';
+import FormHeader from'../header/form__header';
+import {Container, Row, Col} from 'react-bootstrap';
+import {Link} from 'react-router-dom';
+import Jumbotron from 'react-bootstrap/Jumbotron';
+import Form from 'react-bootstrap/Form';
+import './forms.css'
+import {signIn} from '../../actions/index';
+import history from '../../history'
+import {toast, ToastContainer} from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+class login extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            email:"",
+            password:"",
+            errors:{
+                email:"",
+                password:""
+            }
+        }
+    }
+    componentDidMount(){
+        if(this.props.foodRequest.error){
+            this.notifyFail("You need to login first to continue");
+        }
+    }
+    handleInputChange=(event)=>{
+        const target = event.target;
+        const name = target.name;
+        this.setState({
+          [name]: event.target.value
+        });
+    }
+    validateForm = (data)=>{
+        const {email, password} = data;
+        let emailError="",passwordError="", error=false;
+        if(!email){
+            emailError = "Email is required";
+            error = true;            
+        }
+        else if(!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(email))
+        {
+            emailError = "Email address is Invalid";
+            error= true;
+        }
+        if(!password.trim())
+        {
+            passwordError="Password is required"
+            error= true;
+        }
+        else if(password.length<5)
+        {
+            passwordError="Length of password must be 5 characters or more"
+            error= true;
+        }
+        
+        this.setState(prevState => ({
+            errors:{
+                email:emailError,
+                password: passwordError
+            }
+        }))
+        
+        return !error;
+    }
+
+    notifyFail = (message) => toast.error(message);
+
+    handleSubmit = async(event)=> {
+        event.preventDefault();
+        const isValid = this.validateForm(this.state);
+        if(isValid){
+            const {email,password} = this.state;
+            console.log({email,password});
+            await this.props.signIn({email, password});
+
+            if(this.props.auth.error){
+                this.notifyFail(this.props.auth.error);
+            }
+
+        }
+        
+    }
+    render(){
+        return(
+        <div>
+            <FormHeader/>
+            <ToastContainer/>
+            <div className="forms__section">
+                <Container>
+                <Jumbotron className="form__content__div">
+                    <div className="form__heading">
+                        <h2>Login</h2>
+                    </div>
+                    <Form>
+                        <Form.Group controlId="formBasicEmail">
+                            <Form.Label><span className="form__icon"><AiOutlineMail/></span>Email address</Form.Label>
+                            <input name="email" className="form-control" type="email" value={this.state.email} placeholder="Enter email" onChange={this.handleInputChange} />
+                            <div className="invalid__feedback">{this.state.errors.email}</div>
+                        </Form.Group>
+
+                        <Form.Group controlId="formBasicPassword">
+                            <Form.Label><span className="form__icon"><RiLockPasswordFill/></span> Password</Form.Label>
+                            <input name="password" className="form-control" type="password" value={this.state.password} placeholder="Password" onChange={this.handleInputChange} />
+                            <div className="invalid__feedback">{this.state.errors.password}</div>
+                        </Form.Group>
+                        <div className="form__btn">
+                            <button className="btn" type="submit" onClick={this.handleSubmit}>
+                                Login
+                            </button>
+                        </div>
+                    </Form>
+                    <div className="form__other__text">
+                        New user? <Link to="/signup">Sign Up</Link>
+                    </div>
+                    </Jumbotron>
+                </Container>
+            </div>
+        </div>
+        );
+    }
+}
+
+const mapStateToProps = (state, ownProps)=>{
+    return({
+        ...ownProps,
+        auth:state.auth, 
+        foodRequest: state.foodRequest
+    })
+
+}
+
+export default connect(mapStateToProps,{signIn})(login);
