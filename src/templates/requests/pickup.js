8dbefb7d73d9440684cb29e8cc27049a4f962717
Deepanshu Jindal <deepu.jindal2002@gmail.com>
2021-05-25 19:23:37 +0530

    modify pickup points

--- src/templates/requests/pickup.js
+++ src/templates/requests/pickup.js
@@ -1,12 +1,9 @@
 import React, { Component } from 'react'
 import {connect} from 'react-redux';
 import FormHeader from'../header/form__header';
-import {Container, Row, Col, Image,Button,Form,Card,CardDeck} from 'react-bootstrap';
-import Select from 'react-select'
+import {Container, Row, Col,Form} from 'react-bootstrap';
 import './pickup.css';
 import {renderCard} from './request_cards.js'
-import {foodTypes} from '../../variables';
-import {GiKnifeFork} from 'react-icons/gi';
 import {fetchRequests} from '../../actions/index'
 // import Input from 'react-select/src/components/input';
 import FloatingLabelInput from 'react-floating-label-input';
@@ -160,7 +157,7 @@ class PickupRequest extends Component {
     render() {
         const {pageNo, limit, tCount} = this.state;
         let mini = (pageNo-1)*limit + 1;
-        if(tCount==0){
+        if(tCount===0){
             mini = 0;
         }
         let maxi = Math.min(pageNo*limit , tCount)
@@ -195,9 +192,11 @@ class PickupRequest extends Component {
                                         value={this.state.postalCode}
                                     />
                                 </Col>
+                                <div className="invalid__feedback">{this.state.formError}</div>
                             </Form.Group>
                             <div style={{textAlign:'right'}}>   
-                                <button className="filter__button" onClick={this.handleFilterButton}>Search Requests</button>
+                                <button className="filter__button reset_filter_btn"  onClick={this.resetFilterButton}>Reset Filter</button>   
+                                <button className="filter__button apply_filter_btn" onClick={this.handleFilterButton}>Search Requests</button>
                             </div>    
                         </div>
 
