b076238b5b0a4444742226b6f789238fd4458f0d
Deepanshu Jindal <deepu.jindal2002@gmail.com>
2021-05-27 22:00:51 +0530

    update profile

---
+++ src/variables.js
@@ -0,0 +1,25 @@
+export const foodTypes = [{value:'Packaged Food',label:'Packaged Food'}, 
+    {value:'Raw Food', label:'Raw Food'}, 
+    {value:'Cooked Food', label:'Cooked Food'}
+];
+
+export const foodOptions ={
+    "Packaged Food":[
+        {value:'Water', label:'Water'}, {value:'Milk', label:'Milk'}
+    ],
+    "Raw Food":[
+        {value:'Vegetables', label:'Vegetables'},
+        {value:'Fruits', label:'Fruits'},
+        {value:'Wheat', label:'Wheat'},
+        {value:'Rice', label:'Rice'},
+        {value:'Pulses', label:'Pulses'},
+        {value:'Meat', label:'Meat'},
+        {value:'Egg', label:'Egg'},
+        {value:'Fish', label:'Fish'}
+    ],
+    "Cooked Food":[
+        {value:'Boiled Rice', label:'Boiled Rice'},
+        {value:'Chappati', label:'Chappati'},
+        {value:'Cooked Vegetable', label:'Cooked Vegetable'}
+    ]
+}
