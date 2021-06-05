ae3694edb1f4dc80d480cbde71e71aba28909458
Deepanshu Jindal <deepu.jindal2002@gmail.com>
2021-06-05 15:04:50 +0530

    user reducer

---
+++ src/reducers/userUpdates.js
@@ -0,0 +1,15 @@
+import {UPDATE_FORM, UPDATE_FAILED} from '../actions/actionTypes';
+const INITIAL_STATE = {
+    error:null,
+    message:null
+}
+export default (state=INITIAL_STATE,action)=>{
+    switch(action.type){
+        case UPDATE_FORM:
+            return{...state, message:action.payload.msg, error:null}
+        case UPDATE_FAILED:
+            return{...state, message:null, error:action.payload.error}
+        default:
+            return state
+    }
+}
