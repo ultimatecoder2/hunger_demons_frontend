a86132bd160332fecd3ab567f57814fe200164e1
Deepanshu Jindal <deepu.jindal2002@gmail.com>
2021-06-09 00:04:16 +0530

    organization reducer added

---
+++ src/reducers/organizations.js
@@ -0,0 +1,16 @@
+import {FETCH_ORGANIZATION_FAILED, FETCH_ORGANIZATION} from '../actions/actionTypes';
+const INITIAL_STATE = {
+    error:null,
+    message:null, 
+    data:null
+}
+export default (state=INITIAL_STATE,action)=>{
+    switch(action.type){
+        case FETCH_ORGANIZATION_FAILED:
+            return{...state, message:null, error:action.payload.error, data:null}
+        case FETCH_ORGANIZATION:
+            return {...state, message:"Success", error:null, data:action.payload}
+        default:
+            return state
+    }
+}
