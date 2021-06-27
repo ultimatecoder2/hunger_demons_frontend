44fbcdf7491b5b1900e8656f20161d8c5a02f6ba
Deepanshu Jindal <deepu.jindal2002@gmail.com>
2021-06-27 10:37:31 +0530

    redux store update

---
+++ src/reducers/index.js
@@ -0,0 +1,22 @@
+import  {combineReducers} from 'redux';
+import authReducer from './authReducer';
+import addOrganizationReducer from './addOrganization';
+import foodRequestReducer from './foodRequests';
+import user_form_updatesReducer from './userUpdates';
+import donateRequests from './donateRequest';
+import needRequests from './needRequest';
+import organizations from './organizations';
+import profile from './profile';
+import resetPassword from './resetPassword';
+
+export default combineReducers({
+    auth:authReducer,
+    addOrganization:addOrganizationReducer,
+    foodRequest:foodRequestReducer,
+    user_form_updates: user_form_updatesReducer,
+    needRequests,
+    donateRequests, 
+    organizations,
+    profile,
+    resetPassword
+});
