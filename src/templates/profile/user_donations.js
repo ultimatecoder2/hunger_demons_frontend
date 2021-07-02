fae63383cd47ed7743192023e504855047d6079c
Deepanshu Jindal <deepu.jindal2002@gmail.com>
2021-07-02 08:57:19 +0530

    update user donations

--- src/templates/profile/user_donations.js
+++ src/templates/profile/user_donations.js
@@ -7,7 +7,9 @@ import '../requests/pickup.css';
 import {renderCard} from '../requests/request_cards.js'
 import {foodTypes} from '../../variables';
 import {GiKnifeFork} from 'react-icons/gi';
-import {fetchUserRequests} from '../../actions/index'
+import {toast, ToastContainer} from 'react-toastify';
+import {fetchUserRequests, deleteFoodRequest} from '../../actions/index'
+import InfiniteScroll from 'react-infinite-scroll-component';
 
 class UserDonations extends Component {
     constructor(props){
@@ -18,87 +20,48 @@ class UserDonations extends Component {
             state:"",
             country:"",
             postalCode:"",
-            pageNo:0,
-            limit:3,
+            limit:6,
             requestType:"Donate",
-            tCount:"",
-            posts:"",
             formError:"",
-            isLast:false
+            isLast:false,
+            hasMore: true,
+            data:[]
         }
     }
-    fetchNextPosts = async ()=>{
-        console.log(this.props);
+
+    fetchData = async()=>{
+        if(!this.state.hasMore) return;
         const {owner} = this.props;
-        let {pageNo, limit, tCount, requestType, city, state, country, postalCode}= this.state;
-        const skip = (pageNo)*limit;
-        let data = {Limit:limit, Skip:skip, requestType, city, state, country, postalCode, owner}
-        await this.props.fetchUserRequests(data);
+        let { data, limit, requestType, city, state, country, postalCode}= this.state;
+        const skip = data.length;
+        let apiData = {Limit:limit, Skip:skip, requestType, city, state, country, postalCode, owner}
+        await this.props.fetchUserRequests(apiData);
         if(this.props.food_Requests.message==="Success"){
-            this.setState({
-                tCount:this.props.food_Requests.data.count,
-                posts: this.props.food_Requests.data.foodRequest,
-                isLast:((skip+limit)>=this.props.food_Requests.data.count),
-                pageNo: this.state.pageNo+1
-            })
+            let newData = this.props.food_Requests.data.foodRequest;
+            if(newData&& newData.length>0){
+                var hasMoreNew;
+                if(newData.length<limit){
+                    hasMoreNew = false;
+                }else{
+                    hasMoreNew = true;
+                }
+                const resData = data.concat(newData)
+                this.setState({
+                    hasMore: hasMoreNew,
+                    data: resData
+                })
+            }else{
+                this.setState({hasMore:false})
+            }
         }
     }
 
-    fetchPrevPosts = async()=>{
-        const {owner} = this.props;
-        let {pageNo, limit, tCount, requestType, city, state, country, postalCode}= this.state;
-        if(pageNo<=1)
-            return;
-        const skip = (pageNo-2)*limit;
-        let data = {Limit:limit, Skip:skip, requestType, city, state, country, postalCode, owner}
-        await this.props.fetchUserRequests(data);
-        if(this.props.food_Requests.message==="Success"){
-            this.setState({
-                tCount:this.props.food_Requests.data.count,
-                posts: this.props.food_Requests.data.foodRequest,
-                isLast:((skip+limit)>=this.props.food_Requests.data.count),
-                pageNo: this.state.pageNo-1
-            })
-        }
-
-    }
-
-    fetchPosts = async()=>{
-        const {owner} = this.props;
-        const {pageNo, limit, requestType, city, state, country, postalCode}= this.state;
-        let data = {Limit:limit, Skip:0, requestType, city, state, country, postalCode, owner}
-        await this.props.fetchUserRequests(data);
-        if(this.props.food_Requests.message==="Success"){
-            this.setState({
-                tCount:this.props.food_Requests.data.count,
-                posts: this.props.food_Requests.data.foodRequest,
-                pageNo:1,
-                isLast:(limit>=this.props.food_Requests.data.count)
-
-            })
-        }
-    }
     componentDidMount = async()=>{
-        await this.fetchNextPosts();
+        await this.fetchData();
     }
     
-    nextPage = async(e)=>{
-        console.log("Next btn click")
-        e.preventDefault();
-        const {tCount, pageNo, limit} = this.state;
-        if(tCount>pageNo*limit){
-            this.fetchNextPosts();
-        }
-    }
-
-    prevPage = async(e)=>{
-        console.log("back btn click")
-        e.preventDefault();
-        const {tCount, pageNo, limit} = this.state;
-        if(pageNo>1){
-            this.fetchPrevPosts();
-        }
-    }
+    notifyFail = (message) => toast.error(message);
+    notifySuccess = (message) => toast.success(message);
     
     handleMultiSelectChange = foodtype => {
         this.setState({ foodtype });
@@ -112,36 +75,56 @@ class UserDonations extends Component {
         });
     }
 
+    deleteFoodPost = async(id)=>{
+        await this.props.deleteFoodRequest({id})
+        console.log(id)
+        if(this.props.foodRequestConfirmation.error){
+            this.notifyFail(this.props.foodRequestConfirmation.error);
+        }else if(this.props.foodRequestConfirmation.message){
+            this.notifySuccess(this.props.foodRequestConfirmation.message);
+            let data = this.state.data
+            let newData = [];
+            for(var i=0;i<data.length;i++){
+                if(data[i]._id !== id){
+                    newData.push(data[i]);
+                }
+            }
+            this.setState({
+                data: newData,
+            }, this.fetchData)
+            
+        }
+    }
+
     filterFormValidation = ()=>{
         const {city, postalCode} = this.state;
         let error=false;
-        if(!city.trim()&&!postalCode.trim()){
+        if(!city&&!postalCode.trim()){
             error = true;
             this.setState({
                 formError:"At least one of the field from 'City' or 'PostalCode' must be filled"
             })
-            return false;
         }else{
             this.setState({
                 formError:""
             })
-            return true;
         }
+        return !error;
     }
 
     handleFilterButton = (e)=>{
         e.preventDefault();
         const isValid = this.filterFormValidation();
         if(isValid){
-            this.fetchPosts();
+            // this.fetchData();
         }
     }
     
     renderRequests = ()=>{
-        const {posts} = this.state;
+        const posts = this.state.data;
         if(posts&&posts.length>0){
             return posts.map((post,  key)=>{
-                return renderCard({post,val:key, authId:this.props.auth.userId})
+                return renderCard({post,val:key, authId:this.props.auth.userId, deleteFoodRequest: (id)=>this.deleteFoodPost(id)})
             })
         }else{
             return(
@@ -152,32 +135,21 @@ class UserDonations extends Component {
     }
 
     render() {
-        const {pageNo, limit, tCount} = this.state;
-        let mini = (pageNo-1)*limit + 1;
-        if(tCount==0){
-            mini = 0;
-        }
-        let maxi = Math.min(pageNo*limit , tCount)
-        
         return (
             <div>
                         
-                        <div>
-                                <p>Showing {mini} - {maxi} of {tCount}</p>
-                        </div>
-                        <div className="navigation__btns">
-                            <button className="page_navigation_btn  btn--prev_page" onClick={this.prevPage} disabled={(this.state.pageNo===1)}>Previous Page</button>
-                            <button className = "page_navigation_btn btn--next_page" onClick={this.nextPage} disabled={this.state.isLast}>Next Page</button>
-                        </div>
-
-                        <div className="requests__content">
-                            {this.renderRequests()}
-                        </div>
-                        
-                        {/* <div className="navigation__btns">
-                            <button className="page_navigation_btn  btn--prev_page" onClick={this.prevPage} disabled={(this.state.pageNo===1)}></button>
-                            <button className = "page_navigation_btn btn--next_page" onClick={this.nextPage} disabled={this.state.isLast}>Next</button>
-                        </div> */}
+                <div className="requests__content">
+                    <InfiniteScroll 
+                        dataLength={this.state.data.length}
+                        next = {this.fetchData}
+                        hasMore = {this.state.hasMore}
+                        loader={<h4>Loading........</h4>}
+                        endMessage={<></>}
+                        className="flex flex-wrap scroll_div_outer"
+                    >
+                        {this.renderRequests()}
+                    </InfiniteScroll>
+                </div>
             </div>
         )
     }
@@ -187,9 +159,10 @@ const mapStateToProps = (state, ownProps)=>{
     return({
         ...ownProps,
         food_Requests:state.donateRequests,
-        auth:state.auth
+        auth:state.auth,
+        foodRequestConfirmation: state.foodRequest
     })
 
 }
 
-export default connect(mapStateToProps,{fetchUserRequests})(UserDonations);
+export default connect(mapStateToProps,{fetchUserRequests, deleteFoodRequest})(UserDonations);
