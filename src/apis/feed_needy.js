4005a8a5b93e5af007d9e60e5548781a52dcd429
Deepanshu Jindal <deepu.jindal2002@gmail.com>
2021-07-05 19:12:03 +0530

    added backend url

--- src/apis/feed_needy.js
+++ src/apis/feed_needy.js
@@ -1,10 +1,11 @@
 import axios from 'axios';
-
+const baseURL = "https://hunger-demons-backend.herokuapp.com/"
+const devURL = "http://localhost:3001"
 const record =  axios.create({
-    baseURL:'http://localhost:3001'
+    baseURL:baseURL
 })
 const authRecord =(token) => axios.create({
-    baseURL: 'http://localhost:3001',
+    baseURL: baseURL,
     headers: {'Authorization': 'Bearer '+ token}
   });
 export {authRecord, record}
